907a5b4fb4b7194d482bf1f0f2137b8a0e8ed22e
Jesús <jesusgabrieludiz@gmail.com>
2023-08-29 19:42:53 -0400

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import {ProductsModule} from './products/products.module'
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  
   imports: [ConfigModule.forRoot(),GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
