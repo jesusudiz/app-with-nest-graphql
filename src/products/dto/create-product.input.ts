8295e298badc2a6a2a4932a9a2848bc5a9f1fa82
Jesús <jesusgabrieludiz@gmail.com>
2023-09-02 22:23:49 -0400

    created a new DTO

--- src/products/dto/create-product.input.ts
+++ src/products/dto/create-product.input.ts
@@ -3,16 +3,16 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 @InputType()
 export class ProductDTO {
  
-  @Field(() => Int, { description: 'Example field (placeholder)' })
+  @Field(() => Int, { description: ' field id' })
     id:          number
-  @Field(() => Int, { description: 'Example field (placeholder)' })
+  @Field(() => Int, { description: ' field title' })
     title:       string
-  @Field(() => Int, { description: 'Example field (placeholder)' })
+  @Field(() => Int, { description: ' field price' })
     price:       string
-  @Field(() => Int, { description: 'Example field (placeholder)' })
+  @Field(() => Int, { description: ' field category' })
     category:    string
-  @Field(() => Int, { description: 'Example field (placeholder)' })
+  @Field(() => Int, { description: ' field description' })
     description: string
-  @Field(() => Int, { description: 'Example field (placeholder)' })
+  @Field(() => Int, { description: ' field image' })
     image:       string
 }
