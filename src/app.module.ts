7fc5c5aed4f8130b10c27f1850a0a2e7707f9b4b
Jesús <jesusgabrieludiz@gmail.com>
2023-09-02 22:23:14 -0400

    changes on the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,20 @@ import {ProductsModule} from './products/products.module'
 //import { MongooseModule } from '@nestjs/mongoose';
 //import { DatabaseModule } from './database/database.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { AuthModule } from './auth/auth.module';
 @Module({
   
   imports: [ConfigModule.forRoot(),GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     sortSchema: true,
+    playground: false,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
     
-  }),ProductsModule,MongooseModule.forRoot('mongodb://127.0.0.1:27017/app') ],
+  }),ProductsModule,AuthModule,MongooseModule.forRoot('mongodb://127.0.0.1:27017/app') ],
   controllers: [],
-  providers: [],
+   providers: [],
  
 })
 
