dd3e412227b5b06adf40a680347389543829714c
Jesús <jesusgabrieludiz@gmail.com>
2023-09-03 01:48:24 -0300

    changes on teh DTO

--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -4,7 +4,7 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 @InputType()
 export class UpdateAuthInputDTO extends PartialType(CreateAuthInputDTO) {
   @Field(() => Int, { description: ' field Id' })
-  id: number;
+  id?: number;
   @Field(() => Int, { description: ' field name' })
   name: string;
   @Field(() => Int, { description: ' field surname' })
