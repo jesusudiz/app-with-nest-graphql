1ac2fd30b27051beb0a5cb0205d670525fb7ff4c
Jesús <jesusgabrieludiz@gmail.com>
2023-09-24 01:22:27 -0300

    updated  src/auth/dto/login-auth.dto.ts

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -5,7 +5,7 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 export class LoginAuthDTO {
     @IsEmail() 
     email: string
-    @MinLength(8)
-    @MaxLength(12)
-    passwoord:string
+    @MinLength(8, { message: 'Debe contener más de 8 caracteres' })
+    @MaxLength(12, { message: 'No debe superar más de 12 caracteres' })
+    password:string
 }
