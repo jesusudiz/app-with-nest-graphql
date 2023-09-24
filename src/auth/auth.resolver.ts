c54819421dd7caf7c3e98a83821789f3e6d11734
Jesús <jesusgabrieludiz@gmail.com>
2023-09-24 01:21:14 -0300

    created resolver login

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,38 +1,38 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
-import { Auth, AuthResponse } from './entities/auth.entity';
+import { Auth, AuthResponse,UserLoged } from './entities/auth.entity';
 import { CreateAuthInputDTO } from './dto/create-auth.dto';
 import { UpdateAuthInputDTO } from './dto/update-auth.dto';
-
+import {LoginAuthDTO} from './dto/login-auth.dto';
 @Resolver(() => Auth)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
-  @Mutation(() => AuthResponse)
-  createAuth(@Args('createAuthInput') createAuthInput: CreateAuthInputDTO) {
+  @Mutation(() => AuthResponse, { name: 'Register_User' })
+  createAuth(@Args('createAuthInput') registerUser: CreateAuthInputDTO) {
     try {
-      return this.authService.singnIn(createAuthInput);
+      return this.authService.singnIn(registerUser);
     } catch (error) {
       return error.message;
     }
   }
 
-  @Query(() => [Auth], { name: 'auth' })
-  findAll() {
-    return this.authService.findAll();
+  @Query(() => [Auth], { name: 'Restart_Password' })
+  restartPassword() {
+    return this.authService.restartPassword();
   }
 
-  @Query(() => Auth, { name: 'auth' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.authService.findOne(id);
+  @Query(() => UserLoged, { name: 'Login_User' })
+  Login(@Args('LoginAuthDTO', { type: () => Int }) UserLogin: LoginAuthDTO) {
+    return this.authService.Login(UserLogin);
   }
 
-  @Mutation(() => Auth)
+  @Mutation(() => Auth, { name: 'Update_User' })
   updateAuth(@Args('updateAuthInput') updateAuthInput: UpdateAuthInputDTO) {
     return this.authService.update(updateAuthInput.id, updateAuthInput);
   }
 
-  @Mutation(() => Auth)
+  @Mutation(() => Auth, { name: 'Delete_User' })
   removeAuth(@Args('id', { type: () => Int }) id: number) {
     return this.authService.remove(id);
   }
